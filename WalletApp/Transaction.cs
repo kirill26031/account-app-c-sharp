77edcb6a1c48dcd00c64f3b541a49d0490ad1d77
kirill26031 <kirill26031@gmail.com>
2021-03-06 16:11:13 +0200

    Transaction

--- WalletApp/Transaction.cs
+++ WalletApp/Transaction.cs
@@ -8,9 +8,34 @@ namespace WalletApp
 {
     class Transaction
     {
+        Transaction()
+        {
+            _Id = Guid.NewGuid();
+        }
+
         private Guid _Id;
-        private decimal _Sum;
+        private decimal Sum { get; set; }
         private Category _Category;
-        Currency.CurrencyType _CurrencyType;
+        private Currency.CurrencyType _CurrencyType;
+        private string Description { get; set; }
+        private DateTimeOffset DateTime { get; set; }
+
+        public Guid Id
+        {
+            get { return _Id; }
+        }
+
+        public Category Category
+        {
+            get => _Category; 
+            private set => _Category = value;
+        }
+
+        public Currency.CurrencyType CurrencyType
+        {
+            get => _CurrencyType;
+            private set => _CurrencyType = value;
+        }
+
     }
 }
