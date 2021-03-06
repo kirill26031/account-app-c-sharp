b1d71a2c56b60f4a227abc1c64b90972df57488b
Vlad Sable <vladyslav.soboliev@ukma.edu.ua>
2021-03-06 14:17:22 +0200

    Added wallet methods

--- WalletApp/Wallet.cs
+++ WalletApp/Wallet.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +9,43 @@ namespace WalletApp
 {
     public class Wallet
     {
-        public int Add(int a, int b)
-        {
-            return a + b;
+        private Guid _Guid;
+        private string _Name;
+        private decimal _Balance;
+        Currency _Currency;
+        List<Transaction> _Transactions;
+        List<Category> _Categories;
+        Guid _OwnerId;
+
+        public Guid Guid {
+            get => _Guid; 
+            private set => _Guid = value; 
         }
+        public string Name {
+            get => _Name; 
+            private set => _Name = value; 
+        }
+        public decimal Balance { 
+            get => _Balance; 
+            private set => _Balance = value; 
+        }
+        public List<Category> Categories { 
+            get => _Categories; 
+            private set => _Categories = value; 
+        }
+        public Guid OwnerId { 
+            get => _OwnerId; 
+            private set => _OwnerId = value; 
+        }
+        internal Currency Currency { 
+            get => _Currency; 
+            private set => _Currency = value; 
+        }
+        internal List<Transaction> Transactions { 
+            get => _Transactions; 
+            private set => _Transactions = value; 
+        }
+
+
     }
 }
