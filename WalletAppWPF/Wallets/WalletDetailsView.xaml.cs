98f24882706a00b5fe4c1f15a813f89151065f99
kirill26031 <kirill26031@gmail.com>
2021-04-11 02:20:02 +0300

    Finished setting up classes

---
+++ WalletAppWPF/Wallets/WalletDetailsView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace WalletApp.WalletAppWPF.Wallets
+{
+    /// <summary>
+    /// Interaction logic for WalletDetails.xaml
+    /// </summary>
+    public partial class WalletDetailsView : UserControl
+    {
+        public WalletDetailsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
