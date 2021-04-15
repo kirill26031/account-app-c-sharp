46a9bdc078ac302cfd206339dabe24a309825e96
Vlad Sable <vladyslav.soboliev@ukma.edu.ua>
2021-04-15 05:03:44 +0300

    Started work on transaction views

--- WalletAppWPF/Navigation/NavigatableEnums.cs
+++ WalletAppWPF/Navigation/NavigatableEnums.cs
@@ -9,6 +9,7 @@
     public enum MainNavigatableTypes
     {
         Auth,
-        Wallets
+        Wallets,
+        Transactions
     }
 }
---
+++ WalletAppWPF/Transactions/TransactionsViewModel.cs
@@ -0,0 +1,62 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WalletApp.WalletAppWPF.Navigation;
+using WalletApp.WalletAppWPF.Models.Transactions;
+using WalletApp.WalletAppWPF.Services;
+using WalletAppWPF.Transactions;
+using System.Collections.ObjectModel;
+
+namespace WalletApp.WalletAppWPF.Transactions
+{
+    public class TransactionsViewModel : BindableBase, INavigatable<MainNavigatableTypes>
+    {
+        private TransactionService _service;
+        private TransactionDetailsViewModel _currentTransaction;
+        public ObservableCollection<TransactionDetailsViewModel> Transactions { get; set; }
+
+        public TransactionDetailsViewModel CurrentTransaction
+        {
+            get
+            {
+                return _currentTransaction;
+            }
+            set
+            {
+                _currentTransaction = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public TransactionsViewModel()
+        {
+            _service = new TransactionService();
+            Transactions = new ObservableCollection<TransactionDetailsViewModel>();
+            FillTransactions();
+        }
+
+        private async void FillTransactions()
+        {
+
+            foreach (var wallet in await _service.GetTransactions())
+            {
+                Transactions.Add(new TransactionDetailsViewModel(wallet));
+            }
+        }
+
+        public MainNavigatableTypes Type
+        {
+            get
+            {
+                return MainNavigatableTypes.Transactions;
+            }
+        }
+        public void ClearSensitiveData()
+        {
+
+        }
+    }
+}
